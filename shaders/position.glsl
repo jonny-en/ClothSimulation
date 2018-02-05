993974aba1a5cc227d1f058bc4ce4cdde2c12e66
Bastian Bernst <bbernst@uni-koblenz.de>
2018-02-05 16:20:34 +0100

    Restructure Contraints check for better performance

--- shaders/position.glsl
+++ shaders/position.glsl
@@ -5,10 +5,10 @@ uniform float DAMPING;	// Damping coefficent
 uniform float mass;
 uniform float gravity;
 uniform vec3 restLengths; //Spring coefficents
-uniform vec2 pinned[ 2 ];
+uniform vec2 pinned[ 2 ]; // Fixed Points
 
+//Calculate corretion of one point to antother
 vec3 calculateTranslation(vec3 p1, vec2 neighborCoord ,float restLength){
-
 	vec3 p2 = texture2D(texturePosition, neighborCoord/resolution.xy).xyz;
 	vec3 dist = p1 - p2;
 	float dAbs = distance(p1,p2);
@@ -16,6 +16,7 @@ vec3 calculateTranslation(vec3 p1, vec2 neighborCoord ,float restLength){
 	if(dAbs != 0.){
 	s = (restLength - dAbs)/dAbs;
 	}
+	//Fix translation for top row
 	float factor = 0.5;
 	for(int i = 0; i< 2; i++){
 		if(neighborCoord.y == pinned[i].y + 0.5 && neighborCoord.x == pinned[i].x + 0.5){
@@ -28,89 +29,84 @@ vec3 calculateTranslation(vec3 p1, vec2 neighborCoord ,float restLength){
 	return t;
 }
 
+//get requested neighbor point
+vec3 getNeighbors(vec2 coord, vec3 restLengths,int i){
+	vec2 neighborCoords[16];
+	 neighborCoords[0] = vec2(1.,0.);
+	 neighborCoords[1] = vec2(0.,1.);
+	 neighborCoords[2] = vec2(-1.,0.);
+	 neighborCoords[3] = vec2(0.,-1.);
+	 neighborCoords[4] = vec2(1., -1.);
+	 neighborCoords[5] = vec2(1.,1.);
+	 neighborCoords[6] = vec2(-1.,1.);
+	 neighborCoords[7] = vec2(-1.,-1.);
+	 neighborCoords[8] = vec2(2.,0.);
+	 neighborCoords[9] = vec2(0.,2.);
+	 neighborCoords[10] = vec2(-2.,0.);
+	 neighborCoords[11] = vec2(0.,-2.);
+	 neighborCoords[12] = vec2(2.,-2.);
+	 neighborCoords[13] = vec2(2.,2.);
+	 neighborCoords[14] = vec2(-2.,2.);
+	 neighborCoords[15] = vec2(-2.,-2.);
+
+// Workaround to access array with const index
+for(int x = 0; x < 16; x++){
+	if(x == i){
+		float tempX = coord.x + neighborCoords[x].x;
+		float tempY = coord.y + neighborCoords[x].y;
+		vec2 neighbor = vec2(tempX,tempY);
+
+		if(tempX >= 0.5 && tempX <= resolution.x - 0.5 && tempY >= 0.5 && tempY <= resolution.y - 0.5 ){
+
+			if(x < 4){
+				if(!(neighborCoords[x].x == 0.)){
+					return vec3(neighbor,restLengths.x);
+				}else {
+					return vec3(neighbor,restLengths.y);
+				}
+			}else if(x < 8){
+				return  vec3(neighbor,restLengths.z);
+			}else if(x < 12){
+				if(!(neighborCoords[x].x == 0.)){
+					return vec3(neighbor,2.*restLengths.x);
+				}else {
+					return vec3(neighbor,2.*restLengths.y);
+				}
+			}else if(x < 16){
+				return vec3(neighbor,2.*restLengths.z);
+			}
+		}}}
+
+
+}
+
+//check, if contraints are broken and apply correction
 vec3 checkConstraints(vec2 coord, vec3 position){
 	vec3 t = vec3(0.0);
 	vec3 p2 = position;
-	float restLengthX = restLengths.x;
-	float restLengthY = restLengths.y;
-	float restLengthDiag = restLengths.z;
-
-//Structural constraints
-	if(!(coord.x == 0.5)) // left border of mesh
-	{
-		t += calculateTranslation(position,  vec2(coord.x-1.,coord.y),restLengthX);
-		//shearing constraints
-		if(!(coord.y == 0.5)){
-			t += calculateTranslation(position,  vec2(coord.x - 1., coord.y - 1.), restLengthDiag);
-		}
-		if(!(coord.y == resolution.y - 0.5)){
-			t += calculateTranslation(position, vec2(coord.x - 1., coord.y + 1.), restLengthDiag);
-		}
-		// Bending checkConstraints
-		if(!(coord.x <= 1.5)){
-			t += calculateTranslation(position, vec2(coord.x - 2.,coord.y), 2.*restLengthX);
-
-			if(!(coord.y <= 1.5)){
-				t += calculateTranslation(position, vec2(coord.x - 2., coord.y - 2.), 2.*restLengthDiag);
-			}
-		
-			if(!(coord.y >=  resolution.y - 1.5)){
-				t += calculateTranslation(position, vec2(coord.x - 2., coord.y + 2.), 2.*restLengthDiag);
-			}
-		}
+vec3 neighbors[16];
+	for(int i = 0; i < 16; i++){
+		neighbors[i].xy = vec2(0.0,0.0);
 	}
-	if(!(coord.x == (resolution.x-0.5))) // right border of mesh
-	{
-		t += calculateTranslation(position, vec2(coord.x+1.,coord.y), restLengthX);
-		//shearing constraints
-
-		if(!(coord.y == 0.5)){
-			t += calculateTranslation(position, vec2(coord.x + 1., coord.y - 1.), restLengthDiag);
-		}
-		 if(!(coord.y == resolution.y - 0.5)){
-			t += calculateTranslation(position, vec2(coord.x + 1., coord.y + 1.), restLengthDiag);
-		}
-
-		if(!(coord.x >= resolution.x - 1.5)){
-			t += calculateTranslation(position, vec2(coord.x + 2.,coord.y), 2.*restLengthX);
-
-			if(!(coord.y <= 1.5)){
-				t += calculateTranslation(position, vec2(coord.x + 2., coord.y - 2.), 2.*restLengthDiag);
-			}
-			if(!(coord.y>= resolution.y - 1.5)){
-				t += calculateTranslation(position, vec2(coord.x + 2., coord.y + 2.), 2.*restLengthDiag);
-			}
-
-		}
-
+	for(int i = 0; i < 16; i++){
+		neighbors[i] = getNeighbors(coord, restLengths, i);
 	}
-	if(!(coord.y == resolution.y-0.5)) // bottom border of mesh
-	{
-		t += calculateTranslation(position, vec2(coord.x,coord.y+1.), restLengthY);
+for(int i = 0; i < 16; i ++){
+	if(neighbors[i].xy != vec2(0.0,0.0)){
+		t+= calculateTranslation(position, neighbors[i].xy, neighbors[i].z);
 	}
-	if(!(coord.y == 0.5)) // top border of mesh
-	{
-		t += calculateTranslation(position, vec2(coord.x,coord.y-1.), restLengthY);
-	}
-
-	if(!(coord.y <= 1.5)){
-		t += calculateTranslation(position, vec2(coord.x,coord.y - 2.), 2.*restLengthY);
-	}
-	if(!(coord.y >= resolution.y - 1.5)){
-		t += calculateTranslation(position, vec2(coord.x,coord.y + 2.), 2.*restLengthY);
-	}
-
+}
 	return t;
 
 }
 
 
 void main()	{
-
 	vec2 uv = gl_FragCoord.xy / resolution.xy;
 	float ownMass = mass;
 
 	vec2 coord = gl_FragCoord.xy;
+	vec3 neighbors[16];
 
 	vec3 position = texture2D( texturePosition, uv ).xyz;
 	vec3 oldPosition = texture2D( textureOldPosition, uv ).xyz;
@@ -124,11 +120,11 @@ void main()	{
 
 	//fix top row so it does not move
 
-	vec3 force = (gravityVec + wind)*ownMass + velocity*DAMPING;
+	vec3 force = ( gravityVec + wind)*ownMass + velocity*DAMPING;
 	vec3 acceleration = force/ownMass;
 	vec3 t;
 	vec3 newPosition = position;
-	
+
 	bool isPinned = false;
 	for(int i = 0; i< 2; i++){
 		if(uv.y == pinned[i].y/resolution.y + 1.0/(2.0*resolution.y) && uv.x == pinned[i].x/resolution.x + 1.0/(2.0*resolution.x)){
